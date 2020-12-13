f43f1cd9b514876f0731f3a70a8ee87be8f6d2ce
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-13 17:32:22 +0300

    Add BCrypt encryption

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,11 +9,15 @@ public class UserService {
   
   UserRepository userRepository;
 
+  PasswordEncoder passwordEncoder;
+
   public UserService(UserRepository userRepository){
     this.userRepository = userRepository;
+    this.passwordEncoder = new BCryptPasswordEncoder();
   }
 
   public User save(User user){
+    user.setPassword(this.passwordEncoder.encode(user.getPassword()));
     return this.userRepository.save(user);
   }
 
