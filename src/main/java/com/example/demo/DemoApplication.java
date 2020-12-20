c2db1a2849265c62004c2023e23ce8e311fc9459
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 15:44:36 +0300

    Add security and endpoints

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/secured")
+	public String secured(){
+		return "This is secured endpoint";
+	}
+
+	
+	@GetMapping("/public")
+	public String pub(){
+		return "This is public endpoint";
+	}
 }
