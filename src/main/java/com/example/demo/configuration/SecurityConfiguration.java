aa9f4bcc4ead3a3d57bc7e2564b231d4ce9edae4
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 16:02:57 +0300

    Add custom entry point

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -1,9 +1,18 @@
 package com.example.demo.configuration;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -11,7 +20,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
-    http.httpBasic();
+    http.httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint(){
+
+      @Override
+      public void commence(HttpServletRequest request, HttpServletResponse response,
+          AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+
+      }
+      
+    });
 
     http
       .authorizeRequests().antMatchers("/secured").authenticated()
