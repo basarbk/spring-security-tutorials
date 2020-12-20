b633cdf50503c75c691e6a6c3da39624e36372c9
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 16:41:08 +0300

    Return UserDetails

--- src/main/java/com/example/demo/configuration/UserAuthService.java
+++ src/main/java/com/example/demo/configuration/UserAuthService.java
@@ -1,9 +1,13 @@
 package com.example.demo.configuration;
 
+import java.util.Collection;
+
 import com.example.demo.user.User;
 import com.example.demo.user.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,10 +22,47 @@ public class UserAuthService implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User inDB = userRepository.findByUsername(username);
-    if(inDB == null) {
+    if (inDB == null) {
       throw new UsernameNotFoundException("User not found");
     }
-    return null;
+    return new UserDetails() {
+
+      @Override
+      public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("user");
+      }
+
+      @Override
+      public String getPassword() {
+        return inDB.getPassword();
+      }
+
+      @Override
+      public String getUsername() {
+        return inDB.getUsername();
+      }
+
+      @Override
+      public boolean isAccountNonExpired() {
+        return true;
+      }
+
+      @Override
+      public boolean isAccountNonLocked() {
+        return true;
+      }
+
+      @Override
+      public boolean isCredentialsNonExpired() {
+        return true;
+      }
+
+      @Override
+      public boolean isEnabled() {
+        return true;
+      }
+      
+    };
   }
   
 }
