4fc69115f7900368e212b2f9caabe836bbf59e65
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 15:58:28 +0300

    Update session creation policy

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.demo.configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -16,6 +17,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       .authorizeRequests().antMatchers("/secured").authenticated()
       .and()
       .authorizeRequests().anyRequest().permitAll();
+
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
   
 }
