9882cbfb976275a5fa4f970934ce2e652f01adb8
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 15:54:42 +0300

    Enable basic auth

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -10,6 +10,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
+    http.httpBasic();
+
     http
       .authorizeRequests().antMatchers("/secured").authenticated()
       .and()
