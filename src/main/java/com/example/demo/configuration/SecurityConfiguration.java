2aaf33f5675115a846927f388a4ef89ba65b1c6e
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 16:10:06 +0300

    Disable csrf

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -19,6 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
 
     http.httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint(){
 
