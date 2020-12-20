d179977853e54e0f5014ae8191b3942bdf66f194
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 16:30:41 +0300

    Query user by username

--- src/main/java/com/example/demo/configuration/UserAuthService.java
+++ src/main/java/com/example/demo/configuration/UserAuthService.java
@@ -1,5 +1,9 @@
 package com.example.demo.configuration;
 
+import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,9 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserAuthService implements UserDetailsService {
 
+  @Autowired
+  UserRepository userRepository;
+
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    System.err.println("here" + username);
+    User inDB = userRepository.findByUsername(username);
+    if(inDB == null) {
+      throw new UsernameNotFoundException("User not found");
+    }
     return null;
   }
   
--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -3,5 +3,7 @@ package com.example.demo.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  User findByUsername(String username);
   
 }
