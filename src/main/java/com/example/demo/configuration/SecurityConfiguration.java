c98da3e6cd431546b9683a2277f72026f7461248
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 16:12:02 +0300

    Disable headers

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -20,7 +20,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
-
+    http.headers().disable();
     http.httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint(){
 
       @Override
