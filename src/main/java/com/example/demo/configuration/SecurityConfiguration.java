0e6c69f9c1bacc50d3f8d447bdf893384674ee61
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 16:47:28 +0300

    Set encoding for user details service

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -6,16 +6,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  UserAuthService userAuthService;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -38,6 +44,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       .authorizeRequests().anyRequest().permitAll();
 
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userAuthService).passwordEncoder(new BCryptPasswordEncoder());
   }
   
 }
