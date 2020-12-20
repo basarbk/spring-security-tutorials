8951925937ea46cb0858ada3c1ca877f3def7e36
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-20 15:51:13 +0300

    Add security configuration

---
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.demo.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    http
+      .authorizeRequests().antMatchers("/secured").authenticated()
+      .and()
+      .authorizeRequests().anyRequest().permitAll();
+  }
+  
+}
