6e24764692283312d44de9e65b9c27fec3b106db
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2021-03-14 00:15:12 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//teste
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
